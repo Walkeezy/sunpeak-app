69da148f3865462ad862ca4ff797c0f33437e7e1
Kevin Walker <kevin@smartive.ch>
2023-01-27 09:48:44 +0100

    feat: switch to rlayers, add some test cams

--- next.config.js
+++ next.config.js
@@ -1,3 +1,2 @@
-const nextConfig = {
-  swcMinify: false,
-};
+const withTranspile = require("next-transpile-modules")(["ol", "rlayers"]);
+module.exports = withTranspile({ experimental: { esmExternals: "loose" } });
--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,53 +1,48 @@
-import { defaults as defaultControls, ScaleLine } from "ol/control";
-import TileLayer from "ol/layer/Tile";
-import Map from "ol/Map";
-import XYZ from "ol/source/XYZ";
-import View from "ol/View";
-import { useEffect, useRef, useState } from "react";
+import { Point } from "ol/geom";
+import "ol/ol.css";
 import { fromLonLat } from "ol/proj.js";
+import {
+  RFeature,
+  RLayerTile,
+  RLayerVector,
+  RMap,
+  ROSM,
+  ROverlay,
+} from "rlayers";
+import cams from "../data/cams.json";
 
-export const SunpeakMap = function () {
-  const [map, setMap] = useState<Map>(undefined);
-  const mapElement = useRef<HTMLDivElement>(null);
-  const mapRef = useRef<Map>();
-  mapRef.current = map;
-
-  const chur = [9.533333, 46.85];
-  const churWebMercator = fromLonLat(chur);
-
-  useEffect(() => {
-    const initialMap = new Map({
-      target: mapElement.current,
-      controls: defaultControls().extend([
-        new ScaleLine({
-          units: "metric",
-        }),
-      ]),
-      layers: [
-        new TileLayer({
-          source: new XYZ({
-            url: `https://wmts.geo.admin.ch/1.0.0/ch.swisstopo.pixelkarte-farbe/default/current/3857/{z}/{x}/{y}.jpeg`,
-          }),
-        }),
-      ],
-      view: new View({
-        center: churWebMercator,
-        zoom: 11,
-        minZoom: 9,
-        maxZoom: 13,
-        enableRotation: false,
-      }),
-    });
-    setMap(initialMap);
-  }, []);
+export default function SunpeakMap(): JSX.Element {
+  console.log({ cams });
 
   return (
-    <div
-      id="map"
-      ref={mapElement}
-      style={{ height: "100%", width: "100%" }}
-    ></div>
+    <RMap
+      className="h-full w-full"
+      initial={{
+        center: fromLonLat([9.533333, 46.85]),
+        zoom: 10,
+      }}
+      enableRotation={false}
+      minZoom={8}
+      maxZoom={13}
+    >
+      <RLayerTile url="https://wmts.geo.admin.ch/1.0.0/ch.swisstopo.pixelkarte-farbe/default/current/3857/{z}/{x}/{y}.jpeg" />
+      <RLayerVector zIndex={10}>
+        {cams.map((cam) => (
+          <RFeature
+            key={cam.id}
+            geometry={
+              new Point(fromLonLat([cam.location.lng, cam.location.lat]))
+            }
+          >
+            <ROverlay className="bg-white p-1 rounded">
+              <img src={cam.url} width="120" alt={cam.name}></img>
+            </ROverlay>
+          </RFeature>
+        ))}
+      </RLayerVector>
+    </RMap>
   );
-};
+}
 
-export default SunpeakMap;
+// Brambrüesch url: https://backend.roundshot.com/cams/587/thumbnail
+// Chur url: https://backend.roundshot.com/cams/151/thumbnail
