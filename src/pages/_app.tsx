e6f34706d26ff51d01ba45c9dac081cd2bd2f735
Kevin Walker <kevin@smartive.ch>
2023-02-02 15:24:04 +0100

    feat: add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
+import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 import "ol/ol.css";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp;
