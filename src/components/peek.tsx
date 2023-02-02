182d4be546c0e23d0baec7ff717dc396749b3776
Kevin Walker <kevin@smartive.ch>
2023-02-02 15:52:40 +0100

    feat: add animation

--- src/components/peek.tsx
+++ src/components/peek.tsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
 import { Webcam } from "../services/sheet";
 import { joinClassNames } from "../utils/joinClassnames";
 import Caption from "./caption";
@@ -8,9 +9,29 @@ type Props = {
 };
 
 export default function Peek({ webcam }: Props): JSX.Element {
+  const imageRef = useRef<HTMLImageElement>(null);
+  const wrapperRef = useRef<HTMLDivElement>(null);
+  const [pauseAnimation, setPauseAnimation] = useState(false);
+
   const imgUrl = webcam.forceReload
     ? webcam.fullsize + "?" + Date.now()
     : webcam.fullsize;
+
+  useEffect(() => {
+    if (webcam.panorama && imageRef.current && wrapperRef.current) {
+      const interval = setInterval(() => {
+        if (imageRef.current && wrapperRef.current) {
+          wrapperRef.current.scrollLeft += 1;
+        }
+      }, 1000 / 60);
+
+      if (pauseAnimation) {
+        clearInterval(interval);
+      }
+
+      return () => clearInterval(interval);
+    }
+  }, [pauseAnimation]);
 
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center">
@@ -19,12 +40,18 @@ export default function Peek({ webcam }: Props): JSX.Element {
         className="relative border-[3px] border-white bg-white rounded-xl shadow-2xl"
       >
         <div
+          ref={wrapperRef}
+          onTouchStart={() => setPauseAnimation(true)}
           className={joinClassNames([
-            "w-[95vw] h-[40vh] lg:h-[80vh] overflow-auto rounded-xl",
+            "w-[95vw] h-[38vh] lg:h-[80vh] overflow-auto rounded-xl",
             !webcam.panorama && "max-w-[1000px]",
           ])}
         >
-          <img src={imgUrl} className="w-auto h-full max-w-none" />
+          <img
+            ref={imageRef}
+            src={imgUrl}
+            className="w-auto h-full max-w-none"
+          />
         </div>
         <Caption name={webcam.name} link={webcam.link} />
       </motion.div>
