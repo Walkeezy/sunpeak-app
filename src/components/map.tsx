94ddfd128e0e544e91a7b7ba0ae86d2976ca5b5f
Kevin Walker <kevin@smartive.ch>
2023-02-13 22:20:16 +0100

    fix: add all necessary deps for useMemo

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -115,7 +115,7 @@ export default function Map({
                 togglePeek={() => togglePeek(webcam)}
               />
             ),
-            [webcam]
+            [webcam, activeWebcam, refreshQuery, designTokens]
           )
         )}
       </RLayerVector>
