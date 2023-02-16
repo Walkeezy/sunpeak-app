533fea44dfa5b2026b43f211da8cced94b0a06aa
Kevin Walker <kevin@smartive.ch>
2023-02-16 18:51:53 +0100

    fix: use empty webcamData for cypress

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,6 +1,6 @@
 describe('Map', () => {
   it('should display the basic map', () => {
-    cy.visit('/info');
-    cy.get('[data-test-id="info-page"]').should('exist');
+    cy.visit('/');
+    cy.get('[data-test-id="index-page"]').should('exist');
   });
 });
