1ac87f38433062e530218e887692aee395700fe8
Kevin Walker <kevin@smartive.ch>
2023-02-16 14:40:07 +0100

    fix: cypress test & types

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'cypress';
+import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: "http://localhost:3000",
+  },
+
+  screenshotOnRunFailure: false,
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+    },
   },
 });
--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,18 +1,9 @@
-describe("Navigation", () => {
-  it("should navigate to the info page", () => {
-    // Start from the index page
-    cy.visit("/");
-
-    // Find a link with an href attribute containing "about" and click it
+describe('Navigation', () => {
+  it('should navigate to the info page', () => {
+    cy.visit('/');
     cy.get('a[href*="info"]').click();
-
-    // The new url should include "/about"
-    cy.url().should("include", "/info");
-
-    // The new page should contain an h1 with "About page"
-    cy.get("h1").contains(
-      "This interactive map displays webcams from all over Switzerland"
-    );
+    cy.url().should('include', '/info');
+    cy.get('p').contains('This interactive map displays webcams from all over Switzerland');
   });
 });
 
