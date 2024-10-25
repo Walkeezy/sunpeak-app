f9274d68ef4cf105f86ca84e41c831761eede233
Kevin Walker <kevin@smartive.ch>
2024-10-25 13:28:15 +0200

    chore: update deps & add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { ReactNode } from 'react';
 import '../styles/globals.css';
 import { lexend } from './fonts';
@@ -10,6 +11,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <main>{children}</main>
       </body>
       <Analytics />
+      <SpeedInsights />
     </html>
   );
 }
